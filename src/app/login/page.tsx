dacac9bfffad8e7901bcb62e91c4fd7b6a08e691
Diretoria de Tecnologia e Inovação Educacional <dtie@seduc.to.gov.br>
2025-08-30 05:05:15 +0000

    Na tela de login onde circulei de verde coloque bem grande a a imagem no

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,16 @@
 import LoginForm from "@/components/login-form";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-[calc(100vh-10rem)] items-center justify-center py-12">
+    <div className="flex flex-col min-h-[calc(100vh-10rem)] items-center justify-center py-12 gap-8">
+       <Image
+          src="/img/centrologo.png"
+          width={400}
+          height={150}
+          alt="Logotipo do Centro de Mídias Educacionais"
+          className="object-contain"
+        />
       <LoginForm />
     </div>
   );
