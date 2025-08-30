889ad713529b1786431c993645d928c7c68bbb13
Diretoria de Tecnologia e Inovação Educacional <dtie@seduc.to.gov.br>
2025-08-30 02:47:33 +0000

    Continua sem estar conectado

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,9 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { config } from "dotenv";
+
+config(); // Load environment variables
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
