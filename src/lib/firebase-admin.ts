9607821cba521e4dc329b647ad126f4b48d2c30a
Diretoria de Tecnologia e Inovação Educacional <dtie@seduc.to.gov.br>
2025-08-31 21:14:50 +0000

    Acabei de aprovar no site publicado e deu erro, mas aqui no firebase deu

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,25 +3,27 @@ import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import { ServiceAccount } from 'firebase-admin';
 
+let adminApp: App;
+let adminDb: Firestore;
+
 const serviceAccount: ServiceAccount = {
-  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID!,
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, '\n'),
 };
 
-let adminApp: App;
 if (!getApps().length) {
     try {
         adminApp = initializeApp({
             credential: cert(serviceAccount)
         });
-    } catch(e: any) {
-        console.error("Failed to initialize Firebase Admin SDK.", e.message);
+    } catch (e: any) {
+        console.error("Firebase Admin SDK initialization failed:", e);
     }
 } else {
-  adminApp = getApps()[0];
+    adminApp = getApps()[0];
 }
 
-const adminDb: Firestore = getFirestore(adminApp!);
+adminDb = getFirestore(adminApp!);
 
 export { adminDb };
