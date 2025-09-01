f14438dd53ac7550426ae12cb3203256f6930dab
Diretoria de Tecnologia e Inovação Educacional <dtie@seduc.to.gov.br>
2025-09-01 01:31:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/formulario-agendamento-recorrente.tsx
+++ src/components/formulario-agendamento-recorrente.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
